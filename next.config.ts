92d139f9d7b3af4b586918b9b174530f1a0c04d9
Cristian Castiblanco <castiblanco.cristian@uniagraria.edu.co>
2026-04-29 07:47:03 -0500

    Forzando despliegue completo

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* Forzamos a Next.js a ser estricto con las rutas */
+  trailingSlash: false, 
+  // Esto ayuda a Vercel a entender mejor la carpeta app
+  distDir: '.next',
 };
 
 export default nextConfig;
